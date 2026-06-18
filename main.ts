8a58817b2fe497ea400f1036fe9d41ddc0d17816
callmehuda <mafatikhul.huda118@smk.belajar.id>
2026-06-18 08:22:48 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,15 @@ import { Solver } from './solver'
 import { createApp } from './server'
 
 const config = loadConfig(process.argv)
-
 const solver = new Solver(config)
 const { start, stop } = createApp(config, solver)
 
 await start()
 
-const shutdown = async (signal: string) => {
-  console.log(`\n[server] ${signal}`)
+const close = async (sig: string) => {
   await stop()
   process.exit(0)
 }
 
-process.on('SIGINT', () => void shutdown('SIGINT'))
-process.on('SIGTERM', () => void shutdown('SIGTERM'))
+process.on('SIGINT', () => void close('SIGINT'))
+process.on('SIGTERM', () => void close('SIGTERM'))
