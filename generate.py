6aac53ba51a80b369c9c9958559ef52db2cfac9d
callmehuda <mafatikhul.huda118@smk.belajar.id>
2026-06-27 23:51:08 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,15 +6,14 @@ async def main():
             "https://cryptifo.com/api/captcha/generate",
             headers={
                 "Accept": "application/json",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-                "Origin": "https://cryptifo.com",
-                "Referer": "https://cryptifo.com/",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
             },
             timeout=aiohttp.ClientTimeout(total=30),
         ) as r:
-            data = await r.json()
-            print(f"status: {r.status}")
-            print(json.dumps(data, indent=2))
+            print(r)
+            #data = await r.json()
+            #print(f"status: {r.status}")
+            #print(json.dumps(data, indent=2))
 
 if __name__ == "__main__":
     asyncio.run(main())
